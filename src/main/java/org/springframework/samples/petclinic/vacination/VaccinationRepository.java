c9a8cf788fa194644573040245e4d15803792be5
Antonio Manuel <antomanu2000@hotmail.es>
2022-01-14 13:39:16 +0100

    Examen

--- src/main/java/org/springframework/samples/petclinic/vacination/VaccinationRepository.java
+++ src/main/java/org/springframework/samples/petclinic/vacination/VaccinationRepository.java
@@ -3,9 +3,18 @@ package org.springframework.samples.petclinic.vacination;
 import java.util.List;
 import java.util.Optional;
 
-public interface VaccinationRepository {
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.CrudRepository;
+
+public interface VaccinationRepository extends CrudRepository<Vaccination,Integer>{
     List<Vaccination> findAll();
-    //List<Vaccine> findAllVaccines();
+
+    @Query("SELECT vac FROM Vaccine vac WHERE vac.name = :name")
+    Vaccine getVaccine(String name);
+
+    @Query("SELECT vac FROM Vaccine vac")
+    List<Vaccine> findAllVaccines();
+
     Optional<Vaccination> findById(int id);
     Vaccination save(Vaccination p);
 }
--- src/main/java/org/springframework/samples/petclinic/vacination/Vaccine.java
+++ src/main/java/org/springframework/samples/petclinic/vacination/Vaccine.java
@@ -1,5 +1,13 @@
 package org.springframework.samples.petclinic.vacination;
 
+import javax.persistence.Entity;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotNull;
+
+import org.springframework.samples.petclinic.model.NamedEntity;
 import org.springframework.samples.petclinic.pet.PetType;
 
 import lombok.Getter;
@@ -7,9 +15,15 @@ import lombok.Setter;
 
 @Getter
 @Setter
-public class Vaccine {
-    Integer id;
-    String name;
+@Entity
+@Table(name = "vaccines")
+public class Vaccine extends NamedEntity{
+    
+    @Min(0)
     Double price;
+
+    @ManyToOne(optional = false)
+    @NotNull
+	@JoinColumn(name = "pet_type_id")
     PetType petType;
 }
